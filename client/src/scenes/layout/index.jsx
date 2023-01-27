9abbd2aba4ed8755939936e47b4ce59670b310d1
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-01-26 18:05:53 -0800

    server and client folders

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import NavBar from "components/NavBar";
+import Sidebar from "components/Sidebar";
+
+function Layout() {
+  const isNonMobile = useMediaQuery("(min-width: 600px"); // gives us a boolean, if min width is achieved
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+
+      <Box>
+        <NavBar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default Layout;
