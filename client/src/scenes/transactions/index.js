05e1becabda5e88bd50bba8f7b61476e9346e861
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-01-31 12:55:38 -0800

    nearly completed transactions

---
+++ client/src/scenes/transactions/index.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+
+function Transactions() {
+  const theme = useTheme();
+
+  // values to be sent to the backend
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort), // we need to stringify the value of the state so we can put it in the query in the proper JSON format
+    search,
+  });
+
+  const columns = [
+    {
+      field: "_id", // actual data
+      headerName: "ID", // column title
+      flex: 1, // how you want the column to grow, shrink etc
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length, // this gives us the lenght of the products array instead of the array
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="TRANSACTIONS" subtitle="Entire list of transactions" />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+          rowCount={(data && data.total) || 0} // setting the rows to be the total data count
+          pagination
+          page={page} // setting these as the state value we created
+          pageSize={pageSize} // setting these as the state value we created
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          //   components=
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export default Transactions;
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -30,6 +31,47 @@ export const getCustomers = async (req, res) => {
   try {
     const customers = await User.find({ role: "user" }).select("-password"); // we don't want to include the password when we send it to the front
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    // grab values from query string
+    // the sort should look like this: {"field": "userId", "sort": "desc", } // this is what we'll get from material UI datagrid
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    // formatted Sort should look like this { userId: -1 } // this is what mongo db will be able to read
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
+      return sortFormatted;
+    };
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    // Transactions search
+    const transactions = await Transaction.find({
+      $or: [
+        // $or allows us to search for multiple fields
+        { cost: { $regex: new RegExp(search, "i") } }, // if we want to search, we search for the cost param with the search term they inputted. The or allows us to search multiple fields
+        { userId: { $regex: new RegExp(search, "i") } }, // searching for userId
+      ],
+    })
+      .sort(sortFormatted) // sort based on the formatting created above
+      .skip(page * pageSize) // determines the page number/ total transactions to show
+      .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" }, //
+    });
+
+    res.status(200).json({
+      transactions,
+      total,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
