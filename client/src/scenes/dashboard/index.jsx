093d4f4bf0da50ac6b186fd9138799bf914dd425
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-02-03 09:38:13 -0800

    change header

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -59,7 +59,7 @@ function Dashboard() {
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard!" />
 
         <Box>
           <Button
